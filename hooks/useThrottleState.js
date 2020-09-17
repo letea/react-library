2131eea0f5c1ea695df5ab9fffb77b012118d0ee
letea <iamletea@gmail.com>
2020-09-17 18:28:08 +0800

    Hooks: Create useThrottleState hooks and story.

---
+++ hooks/useThrottleState.js
@@ -0,0 +1,45 @@
+// node modules
+import { useState, useCallback, useEffect, useRef } from "react";
+
+const DEFAULT_THROTTLE_TIME = 600;
+const defaultProps = {
+  defaultValue: "",
+  throttleTime: DEFAULT_THROTTLE_TIME
+};
+
+const useThrottleState = props => {
+  const { defaultValue, throttleTime } = { ...defaultProps, ...props };
+  const [value, setValue] = useState(defaultValue);
+  const timer = useRef();
+  const lastTime = useRef();
+  const currentTime = useRef();
+  const setThrottleValue = useCallback(
+    newValue => {
+      if (typeof newValue !== "undefined") {
+        currentTime.current = Date.now();
+        if (lastTime.current && currentTime.current < lastTime.current + throttleTime) {
+            clearTimeout(timer.current);
+            timer.current = setTimeout(() => {
+              lastTime.current = currentTime.current;
+              setValue(newValue);
+            }, throttleTime);
+        } else {
+          clearTimeout(timer.current);
+          lastTime.current = currentTime.current;
+          setValue(newValue);
+        }
+      }
+    },
+    [throttleTime, timer, currentTime, lastTime]
+  );
+
+  useEffect(() => {
+    return () => {
+      clearTimeout(timer.current);
+    };
+  }, []);
+
+  return [value, setThrottleValue];
+};
+
+export default useThrottleState;
---
+++ stories/useThrottleState.stories.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+
+// local modules
+import useThrottleState from "../hooks/useThrottleState";
+import { KnobsContainer, number } from "../hooks/useKnobs";
+import { Input } from "../hooks/useKnobs.style";
+
+storiesOf("Hooks", module).add("useThrottleState", () => {
+  const numberValue = number({ defaultValue: 600, label: "throttleTime" });
+  const [value, setValue] = useThrottleState({
+    defaultValue: "Hello World",
+    throttleTime: numberValue
+  });
+  const onChange = e => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <>
+      <Input defaultValue={value} onChange={onChange} />
+      <div>{`Value: ${value}`}</div>
+      <KnobsContainer />
+    </>
+  )
+});
