c6ea61d9534325768368a43bdc52ea2f1d4e6971
letea <iamletea@gmail.com>
2020-09-17 18:35:14 +0800

    Storybook: Format Example code.

--- stories/Example.stories.js
+++ stories/Example.stories.js
@@ -2,7 +2,5 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 
 storiesOf("Example", module).add("Hello World", () => {
-  return (
-    <h1>Hello World</h1>
-  )
+  return <h1>Hello World</h1>;
 });
