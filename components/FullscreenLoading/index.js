d19168ba49f4cf33ac041f05ce14597ac8bc5480
letea <iamletea@gmail.com>
2020-11-04 14:57:20 +0800

    Components: Create FullscreenLoading component.

---
+++ components/FullscreenLoading/index.js
@@ -0,0 +1,83 @@
+// node modules
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import getRandomString from "@letea/functions/getRandomString";
+
+// local modules
+import createReactDOMEElement from "../../functions/createReactDOMElement";
+import removeReactDOMElement from "../../functions/removeReactDOMElement";
+
+// local files
+import { Wrapper, ExampleSpinner, ExampleTips } from "./style";
+
+const elementId = `fullscreen-loading-${getRandomString(5)}`;
+
+const Loading = ({
+  backgroundColor = "rgba(0, 0, 0, 0.75)",
+  Spinner = null,
+  onEntered = () => {},
+  onClick = () => {},
+  zIndex = 9999
+}) => {
+  useEffect(() => {
+    onEntered && onEntered();
+  }, [onEntered]);
+
+  return (
+    <Wrapper
+      onClick={onClick}
+      backgroundColor={backgroundColor}
+      zIndex={zIndex}
+    >
+      {Spinner && <Spinner />}
+      {!Spinner && (
+        <>
+          <ExampleSpinner />
+          <ExampleTips>CLICK ANYWHERE TO CLOSE</ExampleTips>
+        </>
+      )}
+    </Wrapper>
+  );
+};
+
+Loading.propTypes = {
+  backgroundColor: PropTypes.string,
+  Spinner: PropTypes.elementType,
+  onEntered: PropTypes.func,
+  onClick: PropTypes.func,
+  zIndex: PropTypes.number
+};
+
+const openFullscreenLoading = ({
+  Spinner = null,
+  zIndex = 9999,
+  backgroundColor = "rgba(0, 0, 0, 0.75)"
+} = {}) => {
+  return new Promise(resolve => {
+    createReactDOMEElement({
+      Component: (
+        <Loading
+          backgroundColor={backgroundColor}
+          Spinner={Spinner}
+          onEntered={() => {
+            resolve();
+          }}
+          onClick={() => {
+            removeReactDOMElement(elementId);
+          }}
+          zIndex={zIndex}
+        />
+      ),
+      elementId: elementId
+    });
+  });
+};
+
+const closeFullscreenLoading = () => {
+  return new Promise(resolve => {
+    removeReactDOMElement(elementId);
+    resolve();
+  });
+};
+
+export { openFullscreenLoading, closeFullscreenLoading };
---
+++ components/FullscreenLoading/style.js
@@ -0,0 +1,57 @@
+// node modules
+import styled, { css, keyframes } from "styled-components";
+
+const Wrapper = styled.div`
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.75);
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  justify-content: center;
+  left: 0;
+  position: fixed;
+  top: 0;
+  transform: translate3d(0, 0, 0);
+  width: 100%;
+  z-index: 99990;
+
+  ${props =>
+    props.zIndex &&
+    css`
+      z-index: ${props.zIndex};
+    `}
+
+  ${props =>
+    props.backgroundColor &&
+    css`
+      background-color: ${props.backgroundColor};
+    `}
+`;
+
+const donutSpin = keyframes`
+  0% {
+      transform: rotate(0deg);
+  }
+  100% {
+      transform: rotate(360deg);
+  }
+`;
+
+const ExampleSpinner = styled.div`
+  animation: ${donutSpin} 0.6s linear infinite;
+  border: 4px solid #fff;
+  border-left-color: #1d8b71;
+  border-radius: 50%;
+  display: inline-block;
+  height: 30px;
+  margin-bottom: 0.5rem;
+  width: 30px;
+`;
+
+const ExampleTips = styled.div`
+  color: #fff;
+  font-size: 0.625rem;
+  font-weight: bold;
+`;
+
+export { Wrapper, ExampleSpinner, ExampleTips };
