8c8a71fecbb3b691e0f40181257705fbdf513c64
Leif Lee <leiflee@catchplay.com>
2020-12-13 15:31:21 +0800

    Components: Replace p5 module with p5 cdn links.

--- components/Processing/index.js
+++ components/Processing/index.js
@@ -1,30 +1,47 @@
+const DEFAULT_P5_SCRIPT =
+  "https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.1.9/p5.js";
+const DEFAULT_P5_SOUND_SCRIPT =
+  "https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.1.9/addons/p5.sound.js";
+
 // node modules
 import React, { useEffect, useRef } from "react";
 import PropTypes from "prop-types";
-import P5 from "p5";
+
+// local modules
+import useScript from "../../hooks/useScript";
 
 // local file
 import { Wrapper } from "./style";
 
 const Processing = ({
-  sketch = null,
+  hasSoundLibrary = false,
   isPositionFixed = false,
+  sketch = null,
   zIndex = null
 }) => {
   const ref = useRef();
+  const isP5ScriptLoaded = useScript(DEFAULT_P5_SCRIPT);
+  const isP5SoundScriptLoaded = useScript(
+    hasSoundLibrary && isP5ScriptLoaded ? DEFAULT_P5_SOUND_SCRIPT : ""
+  );
+  const isLibraryLoaded = hasSoundLibrary
+    ? isP5SoundScriptLoaded && isP5SoundScriptLoaded
+    : isP5SoundScriptLoaded;
 
   useEffect(() => {
     let p5;
 
-    if (sketch) {
-      p5 = new P5(sketch, ref.current);
+    if (sketch && isLibraryLoaded) {
+      p5 = new window.p5(sketch, ref.current);
     }
 
     return () => {
-      p5.remove();
-      p5.destroy && p5.destroy();
+      if (p5) {
+        p5.remove();
+        p5.destroy && p5.destroy();
+      }
     };
-  }, []);
+  }, [isLibraryLoaded]);
 
   return (
     <Wrapper ref={ref} isPositionFixed={isPositionFixed} zIndex={zIndex} />
@@ -32,8 +49,9 @@ const Processing = ({
 };
 
 Processing.propTypes = {
-  sketch: PropTypes.func,
+  hasSoundLibrary: PropTypes.bool,
   isPositionFixed: PropTypes.bool,
+  sketch: PropTypes.func,
   zIndex: PropTypes.number
 };
 
