c8420a18e934384976efd3f9a925daae6900a15a
IamLeTea <iamletea@gmail.com>
2020-12-20 13:47:41 +0800

    Storybook: Fix the useKnobs style path of the useThrottleState story.

--- stories/useThrottleState.stories.js
+++ stories/useThrottleState.stories.js
@@ -4,7 +4,7 @@ import { storiesOf } from "@storybook/react";
 // local modules
 import useThrottleState from "../hooks/useThrottleState";
 import { KnobsContainer, number } from "../hooks/useKnobs";
-import { Input } from "../hooks/useKnobs.style";
+import { Input } from "../hooks/useKnobs/style";
 
 storiesOf("Hooks", module).add("useThrottleState", () => {
   const numberValue = number({ defaultValue: 600, label: "throttleTime" });
